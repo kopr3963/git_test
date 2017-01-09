03905de453447d93303804b43c8eaf1754f6d792
김창회 <kopr3963@naver.com>
2017-01-10 01:18:02 +0900

    line clear

--- git_test/git_test/src/git_test/develop.java
+++ git_test/git_test/src/git_test/develop.java
@@ -1,8 +1,8 @@
 package git_test;
 
 public class develop {
-public static void main(String[] args) {
-	System.out.println("develop");
-	System.out.println("develop branch add line");
-}
+	public static void main(String[] args) {
+		System.out.println("develop");
+		System.out.println("develop branch add line");
+	}
 }
