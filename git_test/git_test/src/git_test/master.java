62ae073c92faba82baa72a64bce86ea8c2dc83dd
김창회 <kopr3963@naver.com>
2017-01-10 01:00:49 +0900

    마스터 커밋1

--- git_test/git_test/src/git_test/master.java
+++ git_test/git_test/src/git_test/master.java
@@ -3,5 +3,6 @@ package git_test;
 public class master {
 	public static void main(String[] args) {
 		System.out.println("add master branch commit");
+		System.out.println("master branch 에서 커밋 테스트");
 	}
 }
